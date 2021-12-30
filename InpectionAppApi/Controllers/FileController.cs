5938087ebb77f9b508ed78ad7e3da7717eaa84ab
AJakif <akifahmedjahin@gmail.com>
2021-12-30 13:18:35 +0600

    Get Zip file from API ,extract & save working!

--- InpectionAppApi/Controllers/FileController.cs
+++ InpectionAppApi/Controllers/FileController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using InspectionAppApi.BusinessLayer;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -22,24 +24,46 @@ namespace InpectionAppApi.Controllers
 
         [HttpPost]
         [Route("api/upload")]
-        public string Upload()
+        public async Task<string> Upload([FromForm] FileUploadBO objFile)
         {
 
-            var objFile = HttpContext.Request.Form.Files[0];
+            //var objFile = HttpContext.Request.Form.Files[0];
             try
             {
-                if (objFile.Length > 0)
+                if (objFile.Files.Length > 0)
                 {
-                    string path = _webHostEnv.WebRootPath + "\\uploads\\";
-                    if (!Directory.Exists(path))
+                    string file = Path.GetFileNameWithoutExtension(objFile.Files.FileName);
+                    string Filepath = _webHostEnv.WebRootPath + "\\" + file + "\\";
+                    if (!Directory.Exists(Filepath))
                     {
-                        //creating path
-                        Directory.CreateDirectory(path);
+                        //creating File path
+                        Directory.CreateDirectory(Filepath);
                     }
-                    //unzip here
-                    using FileStream fileStream = System.IO.File.Create(path + objFile.FileName);
-                    objFile.CopyTo(fileStream);
-                    fileStream.Flush();
+                    string Zippath = _webHostEnv.WebRootPath + "\\zip\\";
+                    if (!Directory.Exists(Zippath))
+                    {
+                        //creating Zip path
+                        Directory.CreateDirectory(Zippath);
+                    }
+                    using (var stream = new FileStream(Zippath + objFile.Files.FileName, FileMode.Create))
+                    {
+                        await objFile.Files.CopyToAsync(stream);
+                    }
+
+                    //Delete Zip Folder
+                    ZipFile.ExtractToDirectory(Zippath + objFile.Files.FileName, Filepath);
+
+                    DirectoryInfo di = new DirectoryInfo(Zippath);
+
+                    foreach (FileInfo files in di.GetFiles())
+                    {
+                        files.Delete();
+                    }
+                    foreach (DirectoryInfo dir in di.GetDirectories())
+                    {
+                        dir.Delete(true);
+                    }
+
                     return "Uploaded.";
                 }
                 else
---
+++ InspectionAppApi.BusinessLayer/FileUploadBO.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InspectionAppApi.BusinessLayer
+{
+    public class FileUploadBO
+    {
+        public IFormFile Files { get; set; }
+    }
+}
